2d35a644299f09d7cc3adc95a3397f74e18ee55c
huang kuang <s0936929599@yahoo.com.tw>
2018-05-21 16:42:06 +0800

    Update data_transform.py

--- data_transform.py
+++ data_transform.py
@@ -29,32 +29,32 @@ data=pd.read_csv('C:/Users/BDC/Desktop/PHM08.csv',index_col=None,encoding='big5'
 class raw_data_transform:
     
     def __init__(self,data,nb_engines):
-          self.data=data
-          self.engine=nb_engines
+        self.data=data
+        self.engine=nb_engines
     ###seperate engines   
     def yield_engines(self):
         for i in range(1,self.engine):
             print('現在是第',i,'個engine\n')
-            engines.append(end[end['id']==i])
+            engines.append(self.data[self.data['id']==i])
     ###calcuate RUL        
     def auto_RUL(self):
         for j in range(len(engines)):
             engines[j]=engines[j].assign(AUTO_RUL=max(engines[j]['cycle'])-engines[j]['cycle'])
     ###transorm data to 3-dimenstion       
-    def twod_to_3d(self):
+    def twod_to_3d(self,was):
         for jj in range(0,len(engines)):
             print('-----第',jj+1,'個engine執行中-----\n')
             for ii in range(0,engines[jj].shape[0]):
                     data2d=[]
-                    if(np.array(engines[jj]['cycle'])[ii]-100<0):
-                        zero_paddings=abs(np.array(engines[jj]['cycle'])[ii]-100)
-                        real_data=engines[jj].iloc[0:np.array(engines[jj]['cycle'])[ii],2:26].values
-                        zero_data=np.zeros((zero_paddings,24),dtype=float)    
+                    if(np.array(engines[jj]['cycle'])[ii]-was<0):
+                        zero_paddings=abs(np.array(engines[jj]['cycle'])[ii]-was)
+                        real_data=engines[jj].iloc[0:np.array(engines[jj]['cycle'])[ii],2:(len(self.data.columns)-1)].values #raw data26
+                        zero_data=np.zeros((zero_paddings,(len(self.data.columns)-3)),dtype=float)     # raw data 26-2 #AUTO 2:12
                         data_2d=np.concatenate((zero_data,real_data),axis=0)
                         data_3d.append(data_2d)
-                    if(np.array(engines[jj]['cycle'])[ii]-100>=0):
-                        start=(np.array(engines[jj]['cycle'])[ii]-100)
-                        data_2d=engines[jj].iloc[start:(np.array(engines[jj]['cycle'])[ii]),2:26].values
+                    if(np.array(engines[jj]['cycle'])[ii]-was>=0):
+                        start=(np.array(engines[jj]['cycle'])[ii]-was)
+                        data_2d=engines[jj].iloc[start:(np.array(engines[jj]['cycle'])[ii]),2:(len(self.data.columns)-1)].values
                         data_3d.append(data_2d)
 
 ###define engines as global list
